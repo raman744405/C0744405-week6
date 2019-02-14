73462c7905480926de8efae3b59409e4509d0918
visualstudio <visualstudio@230-PC40>
2019-02-14 11:01:05 -0500

    moused over

--- C0744405/js/main.js
+++ C0744405/js/main.js
@@ -1,23 +1,7 @@
-﻿var rows = prompt("How many rows for your multiplication table:?");
-var cols = prompt("How many columns for your multiplication table:?");
-if (rows == "" || rows == null)
-    rows = 10;
-if (cols == "" || cols == null)
-    cols = 10;
-createTable(rows, cols);
-function createTable(rows, cols) {
-    var j = 1;
-    var output = "<table border='1' width='500' cellspacing='0' cell='5'>";
-    for (i = 1; i <= rows; i++)
-        {
-        output = output + "<tr>";
-        while (j <= cols) {
-            output = output + "<td>" + i * j + "</td>";
-            j = j + 1;
-        }
-        output = output + "<tr>";
-        j = 1;
+﻿function trigger() {
+    document.getElementById("hover").addEventListener("mouseover", popup);
+    function popup() {
+        alert("welcome to my webpage!!");
     }
-    output = output + "</table>";
-    document.write(output);
+
 }
