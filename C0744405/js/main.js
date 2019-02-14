e1fae7b34c00141fb0df5b751a4a375a69e3be89
visualstudio <visualstudio@230-PC40>
2019-02-14 10:08:59 -0500

    table

--- C0744405/js/main.js
+++ C0744405/js/main.js
@@ -1,15 +1,23 @@
-﻿function addNumbers(j) {
-   //var sum = 0;
-    //for (i = 0; i < j; i++) {
-      //  sum += i;
-    //}
-    //return sum;
-    sum = 0;
-    i = 0;
-    while (i < j) {
-        sum += i++;
+﻿var rows = prompt("How many rows for your multiplication table:?");
+var cols = prompt("How many columns for your multiplication table:?");
+if (rows == "" || rows == null)
+    rows = 10;
+if (cols == "" || cols == null)
+    cols = 10;
+createTable(rows, cols);
+function createTable(rows, cols) {
+    var j = 1;
+    var output = "<table border='1' width='500' cellspacing='0' cell='5'>";
+    for (i = 1; i <= rows; i++)
+        {
+        output = output + "<tr>";
+        while (j <= cols) {
+            output = output + "<td>" + i * j + "</td>";
+            j = j + 1;
+        }
+        output = output + "<tr>";
+        j = 1;
     }
-    return sum;
+    output = output + "</table>";
+    document.write(output);
 }
-var inputnumber = prompt("input your number");
-document.write(addNumbers(inputnumber));
