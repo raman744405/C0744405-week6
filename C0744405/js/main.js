fd8bb53b714ef74ddeaeba3f9b1e1145d2025f5c
visualstudio <visualstudio@230-PC40>
2019-02-14 09:05:21 -0500

    C0744405

--- C0744405/js/main.js
+++ C0744405/js/main.js
@@ -1,1 +1,9 @@
-﻿// Your code here!
+﻿function addNumbers(j) {
+    var sum = 0;
+    for (i = 0; i < j; i++) {
+        sum += i;
+    }
+    return sum;
+}
+var inputnumber = prompt("State starting number");
+document.write(addNumbers(inputnumber));
